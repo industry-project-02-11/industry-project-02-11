db22d89e9027bb3955d72bac4899d81a12ea2789
2897robo <2897robo@gmail.com>
2025-05-19 14:00:31 +0900

    feat: add JPA entity modeling and user repository test

--- 11조/6프로그램/apps/backend/src/test/java/com/team11/backend/BackendApplicationTests.java
+++ 11조/6프로그램/apps/backend/src/test/java/com/team11/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.team11.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
